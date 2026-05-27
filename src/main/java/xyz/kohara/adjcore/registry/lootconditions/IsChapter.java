716c1977855c25c2c88a356918fde38a31387512
kohara <kohy.creates@gmail.com>
2026-05-27 12:28:22 +0200

    Fix from chapter loot condition

--- src/main/java/xyz/kohara/adjcore/registry/lootconditions/IsChapter.java
+++ src/main/java/xyz/kohara/adjcore/registry/lootconditions/IsChapter.java
@@ -37,7 +37,7 @@ public class IsChapter implements LootItemCondition {
 
 	public static class Serializer implements net.minecraft.world.level.storage.loot.Serializer<IsChapter> {
 		public void serialize(@NotNull JsonObject jsonObject, @NotNull IsChapter arg, @NotNull JsonSerializationContext jsonSerializationContext) {
-			jsonObject.addProperty("config", arg.chapter);
+			jsonObject.addProperty("from", arg.chapter);
 		}
 
 		public @NotNull IsChapter deserialize(@NotNull JsonObject jsonObject, @NotNull JsonDeserializationContext jsonDeserializationContext) {
