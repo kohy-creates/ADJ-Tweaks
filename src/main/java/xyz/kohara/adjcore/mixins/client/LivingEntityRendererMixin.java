06fedd2ad33592b0ecb273a6f9795c2307603749
kohara <kohy.creates@gmail.com>
2026-07-11 00:51:01 +0200

    Optimize LivingEntityRendererMixin

--- src/main/java/xyz/kohara/adjcore/mixins/client/LivingEntityRendererMixin.java
+++ src/main/java/xyz/kohara/adjcore/mixins/client/LivingEntityRendererMixin.java
@@ -1,67 +1,57 @@
 package xyz.kohara.adjcore.mixins.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xyz.kohara.adjcore.ADJCore;
+import top.theillusivec4.curios.client.render.CuriosLayer;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
 
-	// Adapted from:
-	// https://github.com/ObscuriaLithium/healight/blob/master/common/src/main/java/dev/obscuria/healight/mixin/client/MixinLivingEntityRenderer.java
-	@WrapOperation(
-			method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/client/model/EntityModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V"
-			)
-	)
-	private void healLight(
-			EntityModel<?> instance,
-			PoseStack pose,
-			VertexConsumer consumer,
-			int light,
-			int overlay,
-			float r,
-			float g,
-			float b,
-			float a,
-			Operation<Void> original,
-			@Local(argsOnly = true) LivingEntity entity
-	) {
-		if (entity.adjcore$getHealTime() > 0) {
-			int blockLight = Math.min(15, LightTexture.block(light) + 3);
-			int skyLight = Math.min(15, LightTexture.sky(light) + 3);
-			light = LightTexture.pack(blockLight, skyLight);
-			r = 0.25F;
-			g = 1.0F;
-			b = 0.25F;
-		}
-
-		original.call(instance, pose, consumer, light, overlay, r, g, b, a);
-	}
+//	// Adapted from:
+//	// https://github.com/ObscuriaLithium/healight/blob/master/common/src/main/java/dev/obscuria/healight/mixin/client/MixinLivingEntityRenderer.java
+//	@WrapOperation(
+//			method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+//			at = @At(
+//					value = "INVOKE",
+//					target = "Lnet/minecraft/client/model/EntityModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V"
+//			)
+//	)
+//	private void healLight(
+//			EntityModel<?> instance,
+//			PoseStack pose,
+//			VertexConsumer consumer,
+//			int light,
+//			int overlay,
+//			float r,
+//			float g,
+//			float b,
+//			float a,
+//			Operation<Void> original,
+//			@Local(argsOnly = true) LivingEntity entity
+//	) {
+//		if (entity.adjcore$getHealTime() > 0) {
+//			int blockLight = Math.min(15, LightTexture.block(light) + 3);
+//			int skyLight = Math.min(15, LightTexture.sky(light) + 3);
+//			light = LightTexture.pack(blockLight, skyLight);
+//			r = 0.25F;
+//			g = 1.0F;
+//			b = 0.25F;
+//		}
+//
+//		original.call(instance, pose, consumer, light, overlay, r, g, b, a);
+//	}
 
 	@Inject(
 			method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;)Z",
@@ -73,11 +63,6 @@ public class LivingEntityRendererMixin {
 		if (entity == minecraft.player) {
 			cir.setReturnValue(!minecraft.options.hideGui && minecraft.options.getCameraType() != CameraType.FIRST_PERSON);
 		}
-	}
-
-	@Unique
-	private static boolean adj$isCuriosRenderLayer(RenderLayer renderLayer) {
-		return renderLayer.getClass().getCanonicalName() != null && renderLayer.getClass().getCanonicalName().equals("top.theillusivec4.curios.client.render.CuriosLayer");
 	}
 
 	@SuppressWarnings("unchecked")
@@ -98,7 +83,7 @@ public class LivingEntityRendererMixin {
 			float netHeadYaw,
 			float headPitch
 	) {
-		if (adj$isCuriosRenderLayer(renderLayer) && !(entity instanceof Player)) {
+		if (renderLayer instanceof CuriosLayer<?,?> && !(entity instanceof Player)) {
 			return;
 		}
 		renderLayer.render(poseStack, bufferSource, packedLight, entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
